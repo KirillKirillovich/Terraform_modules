9376e139ae301c543f7d372fcf05e93efa46a576
Kyrylo OLIINYK <kyrylo.oliinyk@smile-ukraine.com>
2024-09-04 17:09:14 +0300

    add rule for 9100

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -84,6 +84,13 @@ dynamic "ingress" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress = {
+    from_port = 9100
+    to_port = 9100
+    protocol = "tcp"
+    cidr_block = var.cidr_block
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
